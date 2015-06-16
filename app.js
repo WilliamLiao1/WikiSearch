cbbc0d12fc6908c1c79a23c83941540bf18dc374
doraDemon <zliao@sfu.ca>
2015-06-15 17:57:18 -0700

    fixing PORT

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
 
-var server = app.listen(3000, function () {
-  console.log('WikiSearch listening on port 3000');
+var server = app.listen(app.get('port'), function () {
+  console.log('WikiSearch listening on port '+app.get('port'));
 });
